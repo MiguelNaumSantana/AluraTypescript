a78a1974ad00894e1ec379864cb6e889ff752791
miguelsantana <platontecdf@gmail.com>
2019-02-03 03:09:11 +0000

    Controller Negociacao

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,10 @@
-const negociacao = new Negociacao(new Date(), 1, 100);
-console.log(negociacao.quantidade);
+/*
+Cria uma instância de
+NegociacaoController para em seguida associar ao
+evento submit do formulário de index.html a chamada do método adiciona.
+
+*/
+const controller = new NegociacaoController();
+document
+    .querySelector('.form')
+    .addEventListener('submit', controller.adiciona.bind(controller));
